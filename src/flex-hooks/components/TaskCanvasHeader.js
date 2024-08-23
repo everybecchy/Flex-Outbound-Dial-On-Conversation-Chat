90c9d92b9162a162c857811add66eab9fe65af87
Everybecch <everybby@gmail.com>
2024-08-23 18:03:53 -0300

    Feat: correction canvas #003

--- src/flex-hooks/components/TaskCanvasHeader.js
+++ src/flex-hooks/components/TaskCanvasHeader.js
@@ -4,10 +4,5 @@ import DialingButton from "../../custom-components/DialingButton/DialingButton";
 export function addDialingButton(flex) {
   flex.TaskCanvasHeader.Content.add(
     <DialingButton key="conversation-dialling-button" />,
-    {
-      sortOrder: 1,
-      if: ({ task }) =>
-        TaskHelper.isCBMTask(task) && task.taskStatus === "assigned",
-    }
   );
 }
